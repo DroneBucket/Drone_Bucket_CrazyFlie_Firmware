6a4fcade4581923a23f206778380eb2c105a8dfd
bitcraze <bitcraze@bitcraze-vm>
2016-02-11 06:07:09 +0100

    Definition dans crtp.h de la macro du nouveau paquet

--- modules/interface/crtp.h
+++ modules/interface/crtp.h
@@ -37,13 +37,14 @@
 #define CRTP_IS_NULL_PACKET(P) ((P.header&0xF3)==0xF3)
 
 typedef enum {
-  CRTP_PORT_CONSOLE     = 0x00,
-  CRTP_PORT_PARAM       = 0x02,
-  CRTP_PORT_COMMANDER   = 0x03,
-  CRTP_PORT_MEM         = 0x04,
-  CRTP_PORT_LOG         = 0x05,
-  CRTP_PORT_PLATFORM    = 0x0D,
-  CRTP_PORT_LINK        = 0x0F,
+  CRTP_PORT_CONSOLE     		= 0x00,
+  CRTP_PORT_PARAM       		= 0x02,
+  CRTP_PORT_COMMANDER   		= 0x03,
+  CRTP_PORT_MEM         		= 0x04,
+  CRTP_PORT_LOG         		= 0x05,
+  CRTP_PORT_COMMANDER_ADVANCED  = 0x0A,
+  CRTP_PORT_PLATFORM    		= 0x0D,
+  CRTP_PORT_LINK        		= 0x0F,
 } CRTPPort;
 
 typedef struct _CRTPPacket
